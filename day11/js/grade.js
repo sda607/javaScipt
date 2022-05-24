61a6c7f6cf20e3b1b45bd408d46950f0814de5eb
sda607 <sda607@naver.com>
2022-05-24 15:42:11 +0900

    day12 소스파일

--- day11/js/grade.js
+++ day11/js/grade.js
@@ -6,17 +6,17 @@ let grade = ""; //문자형 변수 초기화
 if (score >= 90 && score <= 100){
     grade = "A";
 }
-else if(score >= 80 && score < 90){
+else if(score < 90){
     grade = "B";
 }
-else if(score >= 70 && score < 80){
+else if(score < 80){
     grade = "C";
 }
-else if(score >= 60 && score < 70){
+else if(score < 70){
     grade = "D";
 }
 else{
     grade = "F"
 }
 
-document.write("학점은<span>" + grade + "</span>입니다.")
+document.write("학점은<span class = 'accent'>" + grade + "</span>입니다.")
