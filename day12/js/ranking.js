e965565a0fe7388d57b6186178d51b14c629a32d
sda607 <sda607@naver.com>
2022-05-24 17:40:09 +0900

    day12

--- day12/js/ranking.js
+++ day12/js/ranking.js
@@ -1,9 +1,8 @@
-
-//순위에 따른 매달 정하기
-let ranking = 1;
+//순위에 따른 메달 정하기
+let ranking = 2;
 
 switch(ranking){
-    case 1:     //ranking = 1
+    case 1:   //ranking = 1
         document.write("금메달<br>");
         break;
     case 2:
@@ -12,7 +11,25 @@ switch(ranking){
     case 3:
         document.write("동메달<br>");
         break;
-    default:  
-        document.write("메달없음<br>");
-        break;  
+    default:
+        document.write("메달 없음<br>");
+        break;
 }
+
+/*
+초기값 변수
+switch(조건식 or 값){
+case 조건1 or 값:
+    실행문
+    break;
+case 조건2 or 값:
+    실행문
+    break;
+case 조건3 or 값:
+    실행문
+    break;
+default:
+    실행문
+    break;
+}
+*/
