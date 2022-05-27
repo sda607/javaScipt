26a895e3cd431933c9cf959d81ede863d9bcbdfc
sda607 <sda607@naver.com>
2022-05-27 18:19:44 +0900

    day15 리소스파일

---
+++ day15/js/flower.js
@@ -0,0 +1,18 @@
+
+function showText(){
+    //텍스트 보이기
+    let text = document.getElementById('detail');
+    text.style.display = "blcok";
+}
+    // 보기 버튼 숨기기
+    document.getElementById('show');
+    Button.style.display = "none";
+
+function hideText(){
+    let text = document.getElementById('detail');
+    text.style.display = "none";
+}
+
+    // 보기 버튼
+    let Button = document.getElementById('show');
+    text.style.display = "block";
